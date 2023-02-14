b8922b4b4d8a202373bf7bf40ea6d469d1f501ef
ncdev2015 <ncdev2015@gmail.com>
2023-02-13 21:06:06 -0300

    code cleanup

--- 01-gettingstarted/10-polylines-method/example1.py
+++ 01-gettingstarted/10-polylines-method/example1.py
@@ -14,19 +14,18 @@ pts = np.array([
                 ], np.int32)
 
 pts = pts.reshape((-1, 1, 2))
- 
+
 isClosed = True
 
 color = (255, 0, 0)
 thickness = 2
- 
+
 image = cv2.polylines(image, [pts],
                       isClosed, color, thickness)
 
 while(1):
-     
     cv2.imshow('image', image)
     if cv2.waitKey(20) & 0xFF == 27:
         break
-         
+
 cv2.destroyAllWindows()
